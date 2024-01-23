ba6e2ccc47c4b887023d09423ca23dadf80ba7f0
Yuval Shavit <yuvals@wix.com>
2024-01-23 13:50:23 +0200

    add files 2

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.orders.client"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.orders.client"},{"l":"com.orders.Testkit"}];updateSearchResults();
